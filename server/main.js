075a512083cbad298a3cb26dd43ab69b238cf17e
elia <admin@elia.digital>
2020-05-25 13:11:21 +0200

    fixed mobile scroll

--- server/main.js
+++ server/main.js
@@ -28,14 +28,14 @@ const app = express();
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/../dist');
-// app.use(function(req, res, next) {
-//   if(process.env.NODE_ENV=='production'){
-//     if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
-//         res.redirect('https://' + req.get('Host') + req.url);
-//     }
-//   }
-//   next();
-// });
+app.use(function(req, res, next) {
+  if(process.env.NODE_ENV=='production'){
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+  }
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // Point static path to dist
